ab15bc9597020c1930ecb1e2f85019e631a22235
Sanjana Yadav <sanjanayadav3952@gmail.com>
2026-05-18 08:50:16 +0530

    initial

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,12 @@ import leadRoutes from './routes/leadRoutes';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://service-hive-assignment-client.vercel.app',
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
