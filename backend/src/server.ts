691aba6f9230c3ea1bfa2c7e96aca9c558ff0588
Sanjana Yadav <sanjanayadav3952@gmail.com>
2026-05-18 08:44:19 +0530

    initial

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,14 +3,7 @@ import app from './app';
 import { connectDB } from './config/db';
 
 dotenv.config();
-const startServer = async () => {
-  await connectDB();
 
-  const PORT = process.env.PORT || 5000;
+connectDB();
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-};
-
-startServer();
+export default app;
